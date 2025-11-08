7ae9a11b2aae47b53c7f11087a2122d069221242
johnstclair <167724101+johnstclair@users.noreply.github.com>
2025-11-07 16:36:01 -0800

    flake: improvements

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "Rust Sound";
+  description = "Vocal Mouse";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
@@ -7,45 +7,68 @@
     oxalica.url = "github:oxalica/rust-overlay";
   };
 
-  outputs = { self, nixpkgs, flake-utils, oxalica }:
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+      oxalica,
+    }:
     with flake-utils.lib;
-    eachSystem allSystems (system:
+    eachSystem allSystems (
+      system:
       let
         pkgs = nixpkgs.legacyPackages.${system}.extend oxalica.overlays.default;
-      in rec {
+      in
+      rec {
 
         packages = {
-          rust-sound-program = let
-            rustPlatform = pkgs.makeRustPlatform {
-              cargo = pkgs.rust-bin.stable.latest.minimal;
-              rustc = pkgs.rust-bin.stable.latest.minimal;
+          default =
+            let
+              rustPlatform = pkgs.makeRustPlatform {
+                cargo = pkgs.rust-bin.stable.latest.minimal;
+                rustc = pkgs.rust-bin.stable.latest.minimal;
+              };
+            in
+            rustPlatform.buildRustPackage rec {
+              pname = "vocal_mouse";
+              version = "0.1.0";
+
+              src = self;
+
+              nativeBuildInputs = with pkgs; [ pkg-config ];
+
+              buildInputs = with pkgs; [
+                alsa-lib.dev
+                xdotool
+                udev.dev
+                xorg.libX11
+                xorg.libXrandr
+                xorg.libXcursor
+                xorg.libxcb
+                xorg.libXi
+                wayland
+                libxkbcommon
+                libxkbcommon.dev
+                vulkan-loader
+                vulkan-tools
+                glfw
+                xorg.xf86videoamdgpu # notice this line might not match your needs or desires
+              ];
+              cargoLock = {
+                lockFile = ./Cargo.lock;
+              };
+              LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath buildInputs;
             };
-          in rustPlatform.buildRustPackage rec {
-            name = "rust-sound";
-            src = self;
-            nativeBuildInputs = with pkgs; [ pkg-config ];
-            buildInputs = with pkgs; [
-              alsa-lib.dev
-              xdotool
-              udev.dev 
-              xorg.libX11
-              xorg.libXrandr
-              xorg.libXcursor
-              xorg.libxcb
-              xorg.libXi
-              wayland
-              libxkbcommon
-              libxkbcommon.dev
-              vulkan-loader
-              vulkan-tools
-              glfw
-              xorg.xf86videoamdgpu  # notice this line might not match your needs or desires
-            ];
-            cargoLock = { lockFile = ./Cargo.lock; };
-            LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath buildInputs;
+        };
+
+        apps = {
+          default = flake-utils.lib.mkApp {
+            drv = self.packages.${system}.default;
           };
         };
-        defaultPackage = packages.rust-sound-program;
+
         formatter = pkgs.nixfmt;
-      });
+      }
+    );
 }
